2e41b23cd64b325382a5eaeba0fd145cc19f334a
João Victor Lacerda de Queiroz <victorqueiro90@outlook.com>
2022-09-28 14:39:27 -0300

    correção

--- test/UserService.test.ts
+++ test/UserService.test.ts
@@ -1,6 +1,6 @@
 import DatabaseConfig from "../src/configurations/DatabaseConfig";
 import UserService from "../src/entities/User/UserService";
-import User from "../src/types/User"
+import User from "../src/types/User";
 
 describe("should be test the user service methods", ()=>{
 
@@ -8,12 +8,12 @@ describe("should be test the user service methods", ()=>{
         email:"victor.queiroz@academico.ifpb.edu.br",
         name: "João Victor Lacerda de Queiroz",
         password: "123456"
-    }
+    };
     const userService = new UserService();
 
     beforeAll( async ()=>{
-        await DatabaseConfig.connect()
-    })
+        await DatabaseConfig.connect();
+    });
 
 
     
@@ -27,35 +27,35 @@ describe("should be test the user service methods", ()=>{
     test("should be get by uuid", async () =>{
         const result = await userService.findByUuid(user._id);
         expect(result).not.toBe(undefined);
-    })
+    });
 
     test("should be get by email", async () =>{
         const result = await userService.findByEmail(user.email);
         expect(result).not.toBe(undefined);
-    })
+    });
 
     test("should be create a rule", async () =>{
         const result = await userService.addRule(user._id, "ADM");
         expect(result).toBe(true);
-    })
+    });
 
     test("should be delete a rule", async () =>{
         const result = await userService.deleteRule(user._id, "ADM");
         expect(result).toBe(true);
-    })
+    });
 
     test("should be get all", async () =>{
         const result = await userService.read();
         expect(result.length).not.toBe(0);
-    })
+    });
 
     test("should be update name", async () =>{
         const result = await userService.updateName(user._id, "novo nome");
         expect(result.name).toBe("novo nome");
-    })
+    });
 
     test("should be delete name", async () =>{
         const result = await userService.delete(user._id);
         expect(result).not.toBe(undefined);
-    })
-})
+    });
+});
